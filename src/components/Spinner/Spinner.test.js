e5effdb8e1cd520cafe2ce88eaefd55cda67edbc
Josh Weaver <joshweaver@breezechms.com>
2021-02-08 14:13:42 -0600

    adds spinner tests

--- src/components/Spinner/Spinner.test.js
+++ src/components/Spinner/Spinner.test.js
@@ -4,9 +4,21 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-import { Default } from "./Spinner.stories";
+import { Default, Light, Dark } from "./Spinner.stories";
 
 it("renders the spinner in the default color", () => {
     render(<Default {...Default.args} />);
     expect(screen.getByTestId("spinner")).toBeInTheDocument();
+});
+
+it("renders the spinner in the light color", () => {
+    render(<Light {...Light.args} />);
+    expect(screen.getByTestId("spinner")).toBeInTheDocument();
+    expect(screen.getByTestId("spinner")).toHaveClass("light");
+});
+
+it("renders the spinner in the dark color", () => {
+    render(<Dark {...Dark.args} />);
+    expect(screen.getByTestId("spinner")).toBeInTheDocument();
+    expect(screen.getByTestId("spinner")).toHaveClass("dark");
 });
