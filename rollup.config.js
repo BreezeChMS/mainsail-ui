3ae439050efb1c5c0aa3c46d28b803dcbb34bb2b
Josh Weaver <30707961+3CordGuy@users.noreply.github.com>
2021-02-12 12:19:05 -0500

    Fixes peer deps (#21)
    * adds eslintignore file
    
    * changes peer deps list
    
    - embeds clsx

--- rollup.config.js
+++ rollup.config.js
@@ -14,9 +14,6 @@ export default {
             sourcemap: true,
             file: pkg.main,
             format: "umd",
-            globals: {
-                clsx: "clsx",
-            },
         },
     ],
     plugins: [
