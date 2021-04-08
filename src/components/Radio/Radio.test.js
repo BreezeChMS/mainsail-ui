da4552494f406363bc5fa49429237a889fd64fde
Josh Weaver <joshweaver@breezechms.com>
2021-04-08 13:24:42 -0400

    adds tests for RadioGroup

--- src/components/Radio/Radio.test.js
+++ src/components/Radio/Radio.test.js
@@ -6,6 +6,7 @@ import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/extend-expect";
 
 import { Basic, DefaultChecked } from "./Radio.stories";
+import { RadioGroup } from "./Radio";
 
 it("renders the radio in the primary state", () => {
     render(<Basic {...Basic.args} text="Nifty" />);
@@ -48,4 +49,44 @@ it("can be rendered disabled", () => {
     render(<Basic {...Basic.args} isDisabled />);
 
     expect(screen.getByRole("radio")).toBeDisabled();
+});
+
+it("renders the radio in a group", () => {
+    render(
+        <RadioGroup>
+            <Basic text="choice1" name="choices" />
+            <Basic text="choice2" name="choices" />
+        </RadioGroup>
+    );
+    expect(screen.getByText("choice1")).toBeInTheDocument();
+    expect(screen.getByText("choice2")).toBeInTheDocument();
+});
+
+it("renders the radio in a group with a label", () => {
+    render(
+        <RadioGroup labelText="Fancy Label">
+            <Basic text="choice1" />
+            <Basic text="choice2" />
+        </RadioGroup>
+    );
+    expect(screen.getByText("choice1")).toBeInTheDocument();
+    expect(screen.getByText("choice2")).toBeInTheDocument();
+    expect(screen.getByText("Fancy Label")).toBeInTheDocument();
+});
+
+it("can only select one in a group", () => {
+    render(
+        <RadioGroup labelText="Fancy Label">
+            <Basic name="choices" text="choice1" />
+            <Basic name="choices" text="choice2" />
+        </RadioGroup>
+    );
+    let choices = screen.getAllByRole("radio");
+    userEvent.click(choices[0]);
+    expect(choices[0]).toBeChecked();
+    expect(choices[1]).not.toBeChecked();
+
+    userEvent.click(choices[1]);
+    expect(choices[0]).not.toBeChecked();
+    expect(choices[1]).toBeChecked();
 });
