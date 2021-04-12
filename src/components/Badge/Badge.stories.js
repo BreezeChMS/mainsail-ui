334eb9745cc2b0582fa3d334b5f39a7086c72387
Josh Weaver <joshweaver@breezechms.com>
2021-04-12 15:49:43 -0400

    cleans up badges a little

--- src/components/Badge/Badge.stories.js
+++ src/components/Badge/Badge.stories.js
@@ -11,7 +11,7 @@ export default {
             type: { color: "string" },
             control: {
                 type: "select",
-                options: ENUMS.colors,
+                options: Object.keys(ENUMS.colors),
             },
         },
         size: {
