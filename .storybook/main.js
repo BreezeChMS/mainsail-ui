20eb6d062f6be3d5f0ba8792056820add4dab657
Josh Weaver <joshweaver@breezechms.com>
2021-04-19 10:48:00 -0400

    sets up module absolute path resolvers

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
     stories: [
         "../src/**/*.stories.mdx",
@@ -8,4 +10,16 @@ module.exports = {
         "@storybook/addon-essentials",
         "@storybook/preset-create-react-app",
     ],
+
+    webpackFinal: async (config, { configType }) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            utility: path.resolve(__dirname, "..", "src", "utility"),
+            styles: path.resolve(__dirname, "..", "src", "styles"),
+            components: path.resolve(__dirname, "..", "src", "components"),
+        };
+
+        // Return the altered config
+        return config;
+    },
 };
