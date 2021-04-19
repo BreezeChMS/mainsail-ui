d1248f3ca7319e01c43a58a23da78a7aa8174683
Josh Weaver <joshweaver@breezechms.com>
2021-04-19 17:26:25 -0400

    adds initial popper/tooltip work

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { usePopper } from "react-popper";
 import { classify } from "utility/classify";
 
 import "./Tooltip.scss";
 
+const getOffset = (placement, offset = 8) => {
+    let DIST = offset;
+    const DEFAULT_OFFSET = [0, DIST];
+    const offsetMap = {
+        top: [0, DIST],
+        "top-start": [-DIST, DIST],
+        "top-end": [DIST, DIST],
+        left: [0, DIST],
+        "left-start": [-DIST, DIST],
+        "left-end": [DIST, DIST],
+        right: [0, DIST],
+        "right-start": [-DIST, DIST],
+        "right-end": [DIST, DIST],
+        bottom: [0, DIST],
+        "bottom-start": [-DIST, DIST],
+        "bottom-end": [DIST, DIST],
+    };
+
+    return offsetMap[placement] || DEFAULT_OFFSET;
+};
+
+export const placements = {
+    auto: "auto",
+    top: "top",
+    topStart: "top-start",
+    topEnd: "top-end",
+    left: "left",
+    leftStart: "left-start",
+    leftEnd: "left-end",
+    right: "right",
+    rightStart: "right-start",
+    rightEnd: "right-end",
+    bottom: "bottom",
+    bottomStart: "bottom-start",
+    bottomEnd: "bottom-end",
+};
+
 export const ENUMS = {
-    // Add any prop enums here (see Icon.js for example)
+    placements,
 };
 
 /**
@@ -14,24 +52,110 @@ export const ENUMS = {
 export const Tooltip = ({
     className,
     children,
-    ...props
+    text,
+    offset,
+    modifiers = [],
+    placement,
 }) => {
+    const [referenceElement, setReferenceElement] = useState(null);
+    const [visible, setVisibility] = useState(false);
+    const [popperElement, setPopperElement] = useState(null);
+    const [arrowElement, setArrowElement] = useState(null);
+    const { styles, attributes } = usePopper(referenceElement, popperElement, {
+        placement,
+        modifiers: [
+            { name: "arrow", options: { element: arrowElement } },
+            {
+                name: "offset",
+                enabled: true,
+                options: {
+                    offset: getOffset(placement, offset),
+                },
+            },
+            ...modifiers,
+        ],
+    });
+
+    function handleHover(e) {
+        setVisibility(e.type === "mouseover" ? true : false);
+    }
+
     return (
-        <div className={classify('mainsail-tooltip', className)} {...props}>{children}</div>
+        <>
+            {children ? (
+                <span
+                    ref={setReferenceElement}
+                    onMouseOut={handleHover}
+                    onMouseOver={handleHover}>
+                    {children}
+                </span>
+            ) : (
+                <svg
+                    className="tip-icon"
+                    width="12"
+                    height="12"
+                    viewBox="0 0 12 12"
+                    fill="none"
+                    ref={setReferenceElement}
+                    onMouseOut={handleHover}
+                    onMouseOver={handleHover}
+                    xmlns="http://www.w3.org/2000/svg">
+                    <circle
+                        cx="6"
+                        cy="6"
+                        r="5.5"
+                        fill="#E3F2FA"
+                        stroke="#003462"
+                    />
+                    <path
+                        d="M6.05922 3.29971L6.00432 3.29866C5.95726 3.29324 5.91236 3.27129 5.87662 3.23331L5.87666 3.23327L5.87134 3.22779C5.77595 3.12951 5.77597 2.97044 5.87133 2.8722L5.8714 2.87227L5.87816 2.86502C5.91859 2.82168 5.97506 2.79825 6.03395 2.80009L6.04658 2.80048L6.05922 2.80024C6.11965 2.79908 6.17853 2.82254 6.22286 2.86723L6.22274 2.86735L6.23241 2.8766C6.27649 2.91871 6.30091 2.97738 6.29977 3.03802L6.29962 3.04585L6.29972 3.05368C6.30055 3.12054 6.27452 3.17932 6.23242 3.21954L6.22595 3.22572L6.21971 3.23212C6.17561 3.27738 6.11631 3.30081 6.05922 3.29971Z"
+                        fill="#003462"
+                        stroke="#003462"
+                    />
+                    <path
+                        d="M6.19303 8.4806L6.19303 8.74052H6.00658V8.48059V5.42686V5.16693H6.19301L6.19303 8.4806Z"
+                        fill="#003462"
+                        stroke="#003462"
+                    />
+                </svg>
+            )}
+
+            {visible ? (
+                <div
+                    className={classify("mainsail-tooltip", className)}
+                    ref={setPopperElement}
+                    style={styles.popper}
+                    {...attributes.popper}>
+                    {text}
+                    <div
+                        className={classify("tip-arrow", placement)}
+                        ref={setArrowElement}
+                        style={styles.arrow}
+                    />
+                </div>
+            ) : null}
+        </>
     );
 };
 
 Tooltip.propTypes = {
+    /** Object containing popperjs modifiers array for deeper customization of tooltip */
+    modifiers: PropTypes.array,
+    /** Offset distance from trigger element to tooltip */
+    offset: PropTypes.number,
+    /** Location relative to the tip item to display tooltip */
+    placement: PropTypes.oneOf(Object.values(placements)),
+    /** Text to display on tooltip */
+    text: PropTypes.string.isRequired,
     /** (Optional) click handler */
     onClick: PropTypes.func,
     /** Style class to add to Icon wrapper */
     className: PropTypes.string,
 };
 
-Tooltip.defaultProps = {};
+Tooltip.defaultProps = {
+    placement: placements.bottom,
+    offset: 8,
+};
 
-/*
-* Tip: Be sure to attach any prop enums separately for convenience
-* use the plural form of the prop name
-* Tooltip.variants = variants
-*/
+Tooltip.placements = placements;
--- src/components/Tooltip/Tooltip.stories.js
+++ src/components/Tooltip/Tooltip.stories.js
@@ -1,18 +1,42 @@
 import React from "react";
 
 import { Tooltip } from "./Tooltip";
+import { Button } from "components/Button";
 
 export default {
     title: "Overlay/Tooltip",
     component: Tooltip,
-    argTypes: {
-    },
+    argTypes: {},
 };
 
 const Template = (args) => <Tooltip {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-    variant: Tooltip.variant.primary,
-    text: "Click me",
+export const Simple = Template.bind({});
+Simple.args = {
+    placement: Tooltip.placements.bottom,
+    text: "Here are some words that will give more information to the user!",
+};
+Simple.parameters = {
+    layout: "centered",
+    docs: {
+        description: {
+            story: "Provides its own warning/info icon",
+        },
+    },
+};
+
+export const WithChildren = Template.bind({});
+WithChildren.args = {
+    placement: Tooltip.placements.bottom,
+    text: "If you click, you'll regret it!",
+    offset: 20,
+    children: <Button text="Click" intent={Button.intents.danger} />,
+};
+WithChildren.parameters = {
+    layout: "centered",
+    docs: {
+        description: {
+            story: "Provides its own warning/info icon",
+        },
+    },
 };
