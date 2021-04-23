ee3ca4e1446be8b0488040505002d417de2836a4
Josh Weaver <joshweaver@breezechms.com>
2021-04-23 16:34:23 -0400

    adds useful hooks

--- src/utility/hooks.js
+++ src/utility/hooks.js
@@ -1,12 +1,17 @@
-import { memo, useEffect, useRef, useState } from "react";
+import { memo, useEffect, useRef, useState, useMemo } from "react";
 import { createPortal } from "react-dom";
 
-const Portal = ({ id, children }) => {
+/**
+ * WithPortal - render a component to the root of the dom
+ */
+const Portal = ({ id, className, children }) => {
     const el = useRef(
         document.getElementById(id) || document.createElement("div")
     );
     let currentElement = el.current;
     const [dynamic] = useState(!currentElement.parentElement);
+
+    className && currentElement.classList.add(className);
 
     useEffect(() => {
         if (dynamic) {
@@ -22,3 +27,25 @@ const Portal = ({ id, children }) => {
     return createPortal(children, el.current);
 };
 export const WithPortal = memo(Portal);
+
+/**
+ *  useKeydown - detect keypress and respond via a provided callback
+ */
+export const useKeydown = (callback) => {
+    useEffect(() => {
+        document.body.addEventListener("keydown", callback);
+
+        return () => {
+            document.body.removeEventListener("keydown", callback);
+        };
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+};
+
+/**
+ *  useUniqueId - helps generate a unique id for client-side component rendering (useful for aria-roles)
+ */
+let idCounter = 0;
+export const useUniqueId = (prefix) => {
+    const id = useMemo(() => idCounter++, []);
+    return `${prefix}${id}`;
+};
