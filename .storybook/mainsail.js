a7a1adb5161f571cc16dc26eebcfdcafbe9f1f75
Josh Weaver <joshweaver@breezechms.com>
2021-05-21 10:37:58 -0400

    try again [skip chromatic]

--- .storybook/mainsail.js
+++ .storybook/mainsail.js
@@ -1,9 +1,10 @@
 import { create } from "@storybook/theming";
+import logoImg from "../src/assets/mainsail-ui-logo.png";
 
 export default create({
     base: "light",
     brandTitle: "Mainsail UI",
     brandUrl: "https://mainsail-ui.com",
-    brandImage: "mainsail-ui.svg",
+    brandImage: logoImg,
     panelPosition: "right",
 });
