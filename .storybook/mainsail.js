1d4597bbc7e5e9542f06f2dc19c2d01d4b2a98d6
Josh Weaver <30707961+3CordGuy@users.noreply.github.com>
2021-05-21 10:50:17 -0400

    Revert "try again [skip chromatic] (#90)" (#91)
    This reverts commit a7e7788bd31b8ed9a3b91e2279c630e43e318159.

--- .storybook/mainsail.js
+++ .storybook/mainsail.js
@@ -1,10 +1,9 @@
 import { create } from "@storybook/theming";
-import logoImg from "../src/assets/mainsail-ui-logo.png";
 
 export default create({
     base: "light",
     brandTitle: "Mainsail UI",
     brandUrl: "https://mainsail-ui.com",
-    brandImage: logoImg,
+    brandImage: "mainsail-ui.svg",
     panelPosition: "right",
 });
