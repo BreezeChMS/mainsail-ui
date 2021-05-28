c8339938344f9f95db829075be479d988f147a13
Josh Weaver <30707961+3CordGuy@users.noreply.github.com>
2021-05-28 16:01:55 -0400

    Add new icons w/ better search (#103)
    * adds new icons w/ search
    
    * fixes duplicate

---
+++ src/components/Icon/Icon-search.js
@@ -0,0 +1,124 @@
+import { names } from "./Icon";
+import Fuse from "fuse.js";
+
+/**
+ * Since the icon list of names is a bit static
+ * and icons are not always intuitively named
+ *
+ * this map exists to set some overrides for the filter/search
+ * to use more terms to find the proper icon
+ *
+ */
+
+const options = {
+    minMatchCharLength: 2,
+    threshold: 0.3,
+    keys: ["keyword"],
+};
+
+let iconList = [
+    {
+        keyword: "chevron right",
+        icon: names.forward,
+    },
+    {
+        keyword: "chevron left",
+        icon: names.back,
+    },
+    {
+        keyword: "chevron down",
+        icon: names.view,
+    },
+    {
+        keyword: "plus",
+        icon: names.add,
+    },
+    {
+        keyword: "remove",
+        icon: names.close,
+    },
+    {
+        keyword: "envelope",
+        icon: names.email,
+    },
+    {
+        keyword: "exit",
+        icon: names.logout,
+    },
+    {
+        keyword: "dollar",
+        icon: names.bill,
+    },
+    {
+        keyword: "money",
+        icon: names.bill,
+    },
+    {
+        keyword: "dollar",
+        icon: names.bill,
+    },
+    {
+        keyword: "visible",
+        icon: names.public,
+    },
+    {
+        keyword: "hidden",
+        icon: names.private,
+    },
+    {
+        keyword: "pencil",
+        icon: names.edit,
+    },
+    {
+        keyword: "trash",
+        icon: names.delete,
+    },
+    {
+        keyword: "support",
+        icon: names.help,
+    },
+    {
+        keyword: "lock",
+        icon: names.password,
+    },
+    {
+        keyword: "automate",
+        icon: names.bulktask,
+    },
+    {
+        keyword: "bookmark",
+        icon: names.saved,
+    },
+    {
+        keyword: "image",
+        icon: names.photo,
+    },
+    {
+        keyword: "checked",
+        icon: names.task,
+    },
+    {
+        keyword: "sliders",
+        icon: names.preferences,
+    },
+    {
+        keyword: "person",
+        icon: names.user,
+    },
+    {
+        keyword: "person",
+        icon: names.profile,
+    },
+];
+
+// This will add our dynamically generated list from our files/plop scaffolding
+Object.values(names).forEach((icon) => {
+    iconList.push({
+        keyword: icon,
+        icon,
+    });
+});
+
+const fuse = new Fuse(iconList, options);
+
+export default (pattern) => fuse.search(pattern).map(({ item }) => item.icon);
--- src/components/Icon/Icon.stories.js
+++ src/components/Icon/Icon.stories.js
@@ -1,6 +1,9 @@
 import React, { useState, useRef } from "react";
 
 import { Icon as MsIcon } from "./Icon";
+import { useKeydown } from "../../utility/hooks";
+import { ESC_KEY_CODE } from "../../utility/constants";
+import iconSearch from "./Icon-search";
 
 export default {
     title: "Elements/Icon",
@@ -36,8 +39,13 @@ Icon.args = {
 export const FullList = (args) => {
     let [filterVal, setFilterVal] = useState("");
     let copyTextarea = useRef(null);
-    let filterIcons = (icon) =>
-        filterVal ? new RegExp(filterVal, "g").test(icon) : icon;
+    useKeydown((e) => {
+        let isEscPressed = e.keyCode === ESC_KEY_CODE;
+
+        if (isEscPressed) {
+            setFilterVal("");
+        }
+    }, setFilterVal);
 
     let copyToClipboard = (e) => {
         if (!args.allowCopy) {
@@ -48,46 +56,50 @@ export const FullList = (args) => {
         document.execCommand("copy");
     };
 
-    let renderIconList = (LIST) =>
-        LIST.filter(filterIcons)
-            .sort()
-            .map((icon, i) => {
-                return (
+    let renderIconList = (LIST) => {
+        let filteredList = [...LIST];
+        if (filterVal) {
+            filteredList = iconSearch(filterVal);
+        }
+
+        return filteredList.sort().map((icon, i) => {
+            return (
+                <div
+                    key={i}
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        marginBottom: "4px",
+                        transition: "all 0.5s",
+                    }}>
                     <div
-                        key={i}
                         style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                            justifyContent: "center",
+                            fontFamily: "sans-serif",
+                            fontSize: "10px",
                             marginBottom: "4px",
-                            transition: "all 0.5s",
                         }}>
-                        <div
+                        <pre
                             style={{
-                                fontFamily: "sans-serif",
-                                fontSize: "10px",
-                                marginBottom: "4px",
+                                padding: "2px 4px",
+                                background: "#f4f4f4",
+                                borderRadius: "4px",
                             }}>
-                            <pre
-                                style={{
-                                    padding: "2px 4px",
-                                    background: "#f4f4f4",
-                                    borderRadius: "4px",
-                                }}>
-                                {icon && icon}
-                            </pre>
-                        </div>
-                        <Template
-                            onClick={(e) => copyToClipboard(e, icon)}
-                            name={icon}
-                            size={args.size}
-                            isDisabled={args.isDisabled}
-                            color={args.color}
-                        />
+                            {icon && icon}
+                        </pre>
                     </div>
-                );
-            });
+                    <Template
+                        onClick={(e) => copyToClipboard(e, icon)}
+                        name={icon}
+                        size={args.size}
+                        isDisabled={args.isDisabled}
+                        color={args.color}
+                    />
+                </div>
+            );
+        });
+    };
 
     return (
         <div>
@@ -101,12 +113,10 @@ export const FullList = (args) => {
                 <input
                     style={{
                         width: "100%",
-                        fontSize: 12,
-                        borderRadius: "4px",
-                        padding: "4px 8px",
-                        border: "1px solid #ccc",
                         gridColumn: "span 6",
                     }}
+                    onKeyDown={() => {}}
+                    className="py-8 px-20 body-text lg border-neutral-4 rounded-20"
                     value={filterVal}
                     onChange={({ target }) => setFilterVal(target.value)}
                     placeholder="Filter by Icon Name"
