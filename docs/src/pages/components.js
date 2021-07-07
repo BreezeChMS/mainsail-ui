f9dfbf9ac82a1000447a3141c537b362417a7586
Josh Weaver <joshweaver@breezechms.com>
2021-07-06 23:36:42 -0400

    removes unused

--- docs/src/pages/components.js
+++ docs/src/pages/components.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Layout from "@theme/Layout";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./components.module.css";
 
 export default function Components() {
-    const { siteConfig } = useDocusaurusContext();
     return (
         <iframe
             name="storybook"
