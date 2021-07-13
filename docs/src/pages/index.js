8df22e160c552857de0d39528a495b189b20f312
Josh Weaver <joshweaver@breezechms.com>
2021-07-12 20:32:43 -0400

    adds mainsail ui logo to home page

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -4,14 +4,16 @@ import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
+import Logo from "@site/static/img/mainsail-ui.svg";
 import HomepageFeatures from "../components/HomepageFeatures";
+import "../../../src/components/core/index.scss";
 
 function HomepageHeader() {
     const { siteConfig } = useDocusaurusContext();
     return (
         <header className={clsx("hero hero--primary", styles.heroBanner)}>
             <div className="container">
-                <h1 className="hero__title">{siteConfig.title}</h1>
+                <Logo className={styles.mainLogo} />
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
                     <Link
