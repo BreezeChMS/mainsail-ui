ac4825dea8cf552c5a55cdfb30e3f79a060f5adb
Josh Weaver <joshweaver@breezechms.com>
2021-07-12 20:33:20 -0400

    adds forwarding of query string params to storybook iframe

--- docs/src/pages/components.js
+++ docs/src/pages/components.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./components.module.css";
 
 export default function Components() {
+    // Forward any storybook query strings to the iframe for storybook
+    useEffect(() => {
+        if (window) {
+            let params = new URLSearchParams(window.location.search);
+
+            let iframe = document.getElementById("storybook");
+
+            if (iframe && params.get("path")) {
+                console.log(params.get("path"));
+                iframe.src =
+                    "https://master--6000b9fe63cdbd0021082b92.chromatic.com/?path=" +
+                    params.get("path");
+            }
+        }
+    }, []);
+
     return (
         <iframe
+            id="storybook"
             name="storybook"
             className={styles.responsiveIframe}
             src="https://master--6000b9fe63cdbd0021082b92.chromatic.com"
