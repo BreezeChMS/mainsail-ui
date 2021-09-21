d775fd77ca5bc728ddf53b8caca904193830d891
Josh Weaver <30707961+3CordGuy@users.noreply.github.com>
2021-09-21 15:06:31 -0400

    Badge color inversion (#171)
    * adds color inversion support for badges
    
    * adds color inversion support for badge

--- src/components/Badge/Badge.test.js
+++ src/components/Badge/Badge.test.js
@@ -5,17 +5,26 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/extend-expect";
 
-import { Basic, Round, Removable } from "./Badge.stories";
+import { Basic, InvertedColor, Round, Removable } from "./Badge.stories";
 
 it("renders the badge in the primary state", () => {
     render(<Basic {...Basic.args} />);
-    expect(screen.getByText("Badge")).toBeInTheDocument();
+    expect(screen.getByText(Basic.args.text)).toBeInTheDocument();
+});
+
+it("renders the badge in with inverted color", () => {
+    render(<InvertedColor {...InvertedColor.args} />);
+    expect(
+        screen.getByText(InvertedColor.args.text).classList.contains("inverted")
+    ).toBeTruthy();
 });
 
 it("renders the badge with the round style", () => {
     render(<Round {...Round.args} />);
-    expect(screen.getByText("Badge")).toBeInTheDocument();
-    expect(screen.getByText("Badge").classList.contains("round")).toBe(true);
+    expect(screen.getByText(Basic.args.text)).toBeInTheDocument();
+    expect(screen.getByText(Basic.args.text).classList.contains("round")).toBe(
+        true
+    );
 });
 
 it("fires a provided onRemove handler", () => {
