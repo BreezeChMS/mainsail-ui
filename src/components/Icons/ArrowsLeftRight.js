2c09660f8bf6ce5c74a97fe978dd3ea6172d4447
cwalztithely <92600449+cwalztithely@users.noreply.github.com>
2024-06-17 16:02:00 -0700

    Add Associations Icon in React (#247)
    * Create ArrowsLeftRight.js
    
    * Change props to camelCase

---
+++ src/components/Icons/ArrowsLeftRight.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+const SvgArrowsLeftRight = (props) => (
+    <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}>
+        <g
+            clipPath="url(#a)"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2">
+                <path
+                    d="m22 18 4 4-4 4"
+                />
+                <path
+                    d="m6 22h20"
+                />
+                <path
+                    d="m10 14-4-4 4-4"
+                />
+                <path
+                    d="M26 10H6"
+                />
+        </g>
+    </svg>
+);
+export default SvgArrowsLeftRight;
