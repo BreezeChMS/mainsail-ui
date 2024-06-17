6041e078dadb2aa17bfc0638f55b171942e6e1b7
cwalztithely <92600449+cwalztithely@users.noreply.github.com>
2024-06-17 15:47:01 -0700

    Create ArrowsLeftRight.js

---
+++ src/components/Icons/ArrowsLeftRight.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+const SvgArrowsLeftRight = (props) => (
+    <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}>
+        <g
+            clip-path="url(#a)"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2">
+                <path
+                    d="m22 18 4 4-4 4"
+                />
+                <path
+                    d="m6 22h20"
+                />
+                <path
+                    d="m10 14-4-4 4-4"
+                />
+                <path
+                    d="M26 10H6"
+                />
+        </g>
+    </svg>
+);
+export default SvgArrowsLeftRight;
